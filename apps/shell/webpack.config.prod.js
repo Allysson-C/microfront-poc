f20ada76cc7442b051f64dd013f0781d7496d19d
Allysson C. Silva <allysson.cheguera@hotmail.com>
2023-07-31 12:08:14 -0300

    feat: deploy

--- apps/shell/webpack.config.prod.js
+++ apps/shell/webpack.config.prod.js
@@ -24,9 +24,8 @@ const prodConfig = {
    * ]
    */
   remotes: [
-    ['shop', 'http://localhost:4201/'],
-    ['cart', 'http://localhost:4202/'],
-    ['about', 'http://localhost:4203/'],
+    ['cart', 'https://cart-sooty-delta.vercel.app/'],
+    ['about', 'https://microfront-poc-ftad.vercel.app/'],
   ],
 };
 
--- apps/shop/module-federation.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'shop',
-  exposes: {
-    './Module': './src/remote-entry.ts',
-  },
-};
